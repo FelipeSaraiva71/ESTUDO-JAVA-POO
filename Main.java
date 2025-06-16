571b8c12581f7ba2087b701a2e5377cf67f7f214
Felipe Saraiva <felipeluizsaraiva71@gmail.com>
2025-06-16 18:47:54 -0300

    Update Main.java
    commit dell

--- Main.java
+++ Main.java
@@ -1,18 +1,1 @@
-import java.util.Scanner;
-//Sem array entrada única
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-         String nome = Util.validaNome(scanner);
-         int idade =  Util.validaIdade(scanner);
-      String categoria = Util.categoria(idade);
-      String status = Util.status(idade);
-
-      scanner.close();
-
-        System.out.println("Dados: " + nome + idade + categoria + status);
-        System.out.println((idade>= 18)?"APROVADO":"REPROVADO");
-    }
-}
+commit
